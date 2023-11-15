ea99fae8528483b6c01d479b0f098a8e3df4c66d
prov <vprokopov@stud.khpi.ucode-connect.study>
2023-11-15 13:11:21 +0200

    base program

--- inc/uls.h
+++ inc/uls.h
@@ -1,5 +1,17 @@
 #pragma once
 
 #include "libmx.h"
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <sys/xattr.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/acl.h>
+#include <errno.h>
 
--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <uls.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+	DIR* dir = opendir(argv[argc - 1]);
+	struct dirent *test;
+
+	for (test = readdir(dir); test != NULL; test = readdir(dir)) {
+		mx_printstr(test->d_name);
+		mx_printstr(" ");
+	}
+	mx_printstr("\n");
+
 	return 0;
 }
 
